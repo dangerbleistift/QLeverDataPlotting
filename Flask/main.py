cbfcd40da7a3394d2744ad61c4e4f86bfc7624b0
Johannes Gramsch <johannes.gramsch@posteo.de>
2022-02-14 14:16:08 +0100

    Sample Site

---
+++ Flask/main.py
@@ -0,0 +1,25 @@
+from flask import Flask, redirect, url_for, render_template
+import json
+app = Flask(__name__)
+app.debug=True
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/eyes')
+def eyes():
+    data = {}
+    with open('data/eyecolors.json') as f:
+        raw_data = json.load(f)
+        for result in raw_data:
+            data[result['eyeColorLabel']] = result['count']
+    return render_template('table.html', data=data)
+
+@app.route('/d3_sample')
+def d3_sample():
+    return render_template('d3_sample.html')
+
+
+if __name__ == '__main__':
+    app.run()
